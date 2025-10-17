7fe988a43422147ad74dbe6ca40ebe98c49ec39d
Mohit9912 <subhashdhundhara0@gmail.com>
2025-10-17 11:11:28 +0530

    feat: added quiz functionality, created a new json file which stores all the questions

---
+++ quiz_game/app.py
@@ -0,0 +1,31 @@
+# This is a simple quiz game 
+# A user will be asked few questions 
+# For every right answer user get score 1, 
+# and for every wrong answer user get 0. 
+# at last total score will be shown 
+
+import json
+
+with open('questions.json', 'r') as file: # question.json file holds the questions to be asked in this quiz 
+    data = json.load(file)
+
+score = 0
+questions = data['quiz']   
+question_no = len(questions)
+
+print("\n welcome to the quiz!")
+
+print(f"total number or questions are {question_no}")
+for item in questions:
+    user_text = item['question']
+    user_answer = item['answer']
+    
+
+    user_input = input(f"{user_text} : ")
+    if user_input.strip().lower() == user_answer.strip().lower():
+        print("correct answer")
+        score +=1
+    else:
+        print(f"wronganswer!, correct answer is {user_answer}")
+
+print(f"your final score is: {score}")
